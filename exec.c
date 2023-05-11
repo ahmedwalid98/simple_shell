c60f9e306532dda4d6c47c2e6ee8c212867cec88
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-05-11 11:32:39 +0000

    passing the full path to the execv

--- exec.c
+++ exec.c
@@ -3,11 +3,12 @@
 pid_t exec(char **argv)
 {
 int status;
-char *command = NULL;
+char *command = NULL, *real_comm = NULL;
 pid_t pid;
 if (argv)
 {
 command = argv[0];
+real_comm = _getpath(command);
 pid = fork();
 if (pid == -1)
 {
@@ -16,7 +17,7 @@ exit(EXIT_FAILURE);
 }
 else if (pid == 0)
 {
-if (execve(command, argv, NULL) == -1)
+if (execve(real_comm, argv, NULL) == -1)
 {
 write(STDOUT_FILENO, "./hsh: No such file or directory\n", 33);
 exit(EXIT_FAILURE);
