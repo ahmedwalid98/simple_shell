a9065a0e0b8b0531dbffa745e75f0a02bb51ee8b
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-05-11 11:33:43 +0000

    adding the strcat function

--- string_functions.c
+++ string_functions.c
@@ -36,3 +36,25 @@ s++;
 }
 return (len);
 }
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The desireable string
+ * @src: The second string
+ * Return: A string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (s);
+}
